05a50860ccec6a168f4630c06f6ddf5e13c0a1f7
Joy Ghosh <36255129+JoyGhoshs@users.noreply.github.com>
2021-07-04 05:58:49 -0400

    Create img_change.js

---
+++ mad-js/img_change.js
@@ -0,0 +1,4 @@
+Array.prototype.slice.call(
+  document.querySelectorAll('img')).map(function(tag){
+    tag.src = 'https://www.freepnglogos.com/uploads/hacker-png/hacker-interpol-arrests-suspected-anonymous-hackers-motley-5.png';
+});
